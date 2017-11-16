9ecffaad2b9e3d3df7c1a62534ec01c22039332c
Linh LV <linh.codeunited@gmail.com>
2017-11-16 16:18:23 +0700

    Not activate plugin if Gutenberg not activated

--- install.php
+++ install.php
@@ -6,5 +6,10 @@ register_activation_hook(GUTENBERG_ADVANCED_PLUGIN, 'gbadv_plugin_activated');
 // Init plugin
 function gbadv_plugin_activated()
 {
+    // Check if Gutenberg is activated
+    if (!function_exists('register_block_type')) {
+        wp_die('Gutenberg not activated!');
+        exit;
+    }
 
 }
