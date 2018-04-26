99652174c4484b515784a63e87bd666c6ffc63af
Linh LV <linh.codeunited@gmail.com>
2018-04-26 11:41:44 +0700

    Change spacer icon to material design

--- assets/blocks/spacer/block.js
+++ assets/blocks/spacer/block.js
@@ -59,10 +59,17 @@ var AdvgbSpacerBlock = function (_Component) {
     return AdvgbSpacerBlock;
 }(Component);
 
+var spacerBlockIcon = React.createElement(
+    'svg',
+    { fill: '#000000', height: '20', viewBox: '1 2 20 20', width: '20', xmlns: 'http://www.w3.org/2000/svg' },
+    React.createElement('path', { id: 'svg_1', fill: 'none', d: 'm0,0l24,0l0,24l-24,0l0,-24z' }),
+    React.createElement('path', { transform: 'rotate(-90 12.000000000000002,12)', id: 'svg_2', d: 'm7.77,6.76l-1.54,-1.28l-5.41,6.52l5.41,6.52l1.54,-1.28l-4.35,-5.24l4.35,-5.24zm-0.77,6.24l2,0l0,-2l-2,0l0,2zm10,-2l-2,0l0,2l2,0l0,-2zm-6,2l2,0l0,-2l-2,0l0,2zm6.77,-7.52l-1.54,1.28l4.35,5.24l-4.35,5.24l1.54,1.28l5.41,-6.52l-5.41,-6.52z' })
+);
+
 registerBlockType('advgb/spacer', {
     title: __('Spacer'),
     description: __('Create space between blocks.'),
-    icon: 'align-center',
+    icon: spacerBlockIcon,
     category: 'layout',
     keywords: [__('space'), __('spacer'), __('margin')],
     attributes: {
