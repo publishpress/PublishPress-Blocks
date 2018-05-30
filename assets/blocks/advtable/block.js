033fc11e1396c0a77721b74623d8b7782ebe1090
Linh LV <linh.codeunited@gmail.com>
2018-05-30 17:18:17 +0700

    Fix icon size

--- assets/blocks/advtable/block.js
+++ assets/blocks/advtable/block.js
@@ -34,7 +34,7 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 
     var tableBlockIcon = React.createElement(
         "svg",
-        { xmlns: "http://www.w3.org/2000/svg", width: "22", height: "22", viewBox: "2 2 22 22" },
+        { xmlns: "http://www.w3.org/2000/svg", width: "20", height: "20", viewBox: "2 2 22 22" },
         React.createElement("path", { d: "M3 3v18h18V3H3zm8 16H5v-6h6v6zm0-8H5V5h6v6zm8 8h-6v-6h6v6zm0-8h-6V5h6v6z" }),
         React.createElement("path", { d: "M0 0h24v24H0z", fill: "none" })
     );
--- assets/blocks/advtable/block.jsx
+++ assets/blocks/advtable/block.jsx
@@ -6,7 +6,7 @@
     const { PanelBody, PanelColor, BaseControl, RangeControl, SelectControl, IconButton, Toolbar, DropdownMenu } = wpComponents;
 
     const tableBlockIcon = (
-        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="2 2 22 22">
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="2 2 22 22">
             <path d="M3 3v18h18V3H3zm8 16H5v-6h6v6zm0-8H5V5h6v6zm8 8h-6v-6h6v6zm0-8h-6V5h6v6z"/>
             <path d="M0 0h24v24H0z" fill="none"/>
         </svg>
