010ce5a9a3e70d4a5f39957b38cf51090fc484ef
Linh LV <linh.codeunited@gmail.com>
2018-07-30 15:51:03 +0700

    Change search settings function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,40 @@
 (function ( $ ) {
+    $.expr[":"].contains = $.expr.createPseudo(function(arg) {
+        return function( elem ) {
+            return $(elem).text().toLowerCase().indexOf(arg.toLowerCase()) >= 0;
+        };
+    });
+
     $(document).ready(function ( $ ) {
         // Function for searching menus
         $('.ju-menu-search-input').on('input', function () {
-            var searchKey = $(this).val().trim().toLowerCase();
+            $('.ju-right-panel li.ju-settings-option').removeClass('search_result');
+            $('.ju-menu-tabs .tab').show();
 
-            $('.ju-menu-tabs .tab').each(function () {
-                var tabTitle = $(this).text().toLowerCase();
-                if (tabTitle.indexOf(searchKey) > -1) {
-                    $(this).show();
-                } else {
-                    $(this).hide();
+            var searchKey = $(this).val().trim().toLowerCase();
+            if (searchKey === '') {
+                $('.ju-menu-tabs .tab').show();
+                return false;
+            }
+
+            var searchResult = $('.ju-right-panel li.ju-settings-option label:contains("'+searchKey+'")').closest('li.ju-settings-option');
+            var searchParent = searchResult.closest('.ju-content-wrapper');
+            var tabID = [];
+
+            searchResult.each(function () {
+                $(this).addClass('search-result');
+            });
+
+            searchParent.each(function () {
+                tabID.push($(this).attr('id'));
+            });
+
+            $('.ju-menu-tabs .tab a').each(function () {
+                var href = $(this).attr('href').replace(/#/g, '');
+                if (tabID.indexOf(href) < 0) {
+                    $(this).closest('li.tab').hide();
                 }
-            })
+            });
         });
 
         $('.advgb-search-input').on('focus', function () {
