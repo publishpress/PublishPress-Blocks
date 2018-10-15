a6035fd579f95146110c8021bf441f7a13d72d2a
Damien <damien.barrere@gmail.com>
2018-10-15 23:06:41 +0200

    Remove wrong environment var

--- tests/scripts/codeception.sh
+++ tests/scripts/codeception.sh
@@ -18,8 +18,8 @@ function do_tests () {
     echo -e "\n\e[34m\e[1m##### Run tests on php $1 #####\e[0m\n"
     mysql -uroot -h $MYSQL_IP -e "DELETE FROM wp_options WHERE option_value LIKE 'advgb_%';" wordpress
     sshpass -p 'password' ssh -q -o PreferredAuthentications=password -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -a -p 2222 root@$WWW_IP "/root/set_php_version.sh $1; cd /var/www/html/; wp plugin deactivate --allow-root advanced-gutenberg; wp post delete --allow-root \$(wp post list --allow-root --post_type='advgb_profiles' --format=ids) || true; wp post delete --allow-root \$(wp post list --allow-root --post_type='post' --format=ids) || true"
-    codecept run functional --skip-group php5.2 --env local --fail-fast
-    codecept run acceptance --skip-group php5.2 --env local --fail-fast
+    codecept run functional --skip-group php5.2 --fail-fast
+    codecept run acceptance --skip-group php5.2 --fail-fast
 }
 
 for PHP_VERSION in '5.3' '5.4' '5.5' '5.6' '7.0' '7.1' '7.2'
