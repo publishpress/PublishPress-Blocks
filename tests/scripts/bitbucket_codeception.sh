c610ab160193b415a84bb7ae6511432583196f7f
Damien <damien.barrere@gmail.com>
2018-10-15 19:59:04 +0200

    Create folder before upload

--- tests/scripts/bitbucket_codeception.sh
+++ tests/scripts/bitbucket_codeception.sh
@@ -13,6 +13,7 @@ WWW_IP="127.0.0.1"
 MYSQL_IP="127.0.0.1"
 
 # Copy plugin to web server
+sshpass -p 'password' ssh -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -a -p 2222 root@127.0.0.1 "mkdir -p /var/www/html/wp-content/plugins/advanced-gutenberg/"
 sshpass -p 'password' scp -r -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -P 2222 $BITBUCKET_CLONE_DIR/* root@127.0.0.1:/var/www/html/wp-content/plugins/advanced-gutenberg
 
 source ./tests/scripts/codeception.sh
