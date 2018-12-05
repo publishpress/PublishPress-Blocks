8a3868214103fdbdaaf3cf8221e59e6cef575838
Linh LV <linh.codeunited@gmail.com>
2018-12-05 16:44:33 +0700

    Compatible with WP 5.0: fix Map block icon

--- assets/blocks/blocks.js
+++ assets/blocks/blocks.js
@@ -5628,7 +5628,7 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 
     var mapBlockIcon = React.createElement(
         "svg",
-        { xmlns: "http://www.w3.org/2000/svg", width: "20", height: "20", viewBox: "2 2 22 22" },
+        { xmlns: "http://www.w3.org/2000/svg", width: "20", height: "20", viewBox: "2 2 22 22", className: "dashicon" },
         React.createElement("path", { d: "M20.5 3l-.16.03L15 5.1 9 3 3.36 4.9c-.21.07-.36.25-.36.48V20.5c0 .28.22.5.5.5l.16-.03L9 18.9l6 2.1 5.64-1.9c.21-.07.36-.25.36-.48V3.5c0-.28-.22-.5-.5-.5zM15 19l-6-2.11V5l6 2.11V19z" }),
         React.createElement("path", { d: "M0 0h24v24H0z", fill: "none" })
     );
