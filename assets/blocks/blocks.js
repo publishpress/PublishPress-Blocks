2b2599aa78f2032be6591c70087faab0e3acf3bd
Linh LV <linh.codeunited@gmail.com>
2018-12-05 16:41:40 +0700

    Compatible with WP 5.0: fix Image Slider icon not display properly

--- assets/blocks/blocks.js
+++ assets/blocks/blocks.js
@@ -4994,7 +4994,7 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 
     var imageSliderBlockIcon = React.createElement(
         "svg",
-        { xmlns: "http://www.w3.org/2000/svg", width: "20", height: "20", viewBox: "2 2 22 22", className: "editor-block-icon" },
+        { xmlns: "http://www.w3.org/2000/svg", width: "20", height: "20", viewBox: "2 2 22 22", className: "dashicon" },
         React.createElement("path", { fill: "none", d: "M0 0h24v24H0V0z" }),
         React.createElement("path", { d: "M20 4h-3.17L15 2H9L7.17 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zM9.88 4h4.24l1.83 2H20v12H4V6h4.05" }),
         React.createElement("path", { d: "M15 11H9V8.5L5.5 12 9 15.5V13h6v2.5l3.5-3.5L15 8.5z" })
