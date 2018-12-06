bf54004ba59817f38c6b991ead6cf5f6735bc23a
Damien <damien.barrere@gmail.com>
2018-12-06 12:46:12 +0100

    Update tests with latest enhancements

--- tests/scripts/codeception.sh
+++ tests/scripts/codeception.sh
@@ -12,14 +12,34 @@ sshpass -p 'password' ssh -q -o PreferredAuthentications=password -o UserKnownHo
 sshpass -p 'password' scp -q -r -o PreferredAuthentications=password -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -P 2222 "$( pwd )"/tests/_data/block root@$WWW_IP:/var/www/html/wp-content/plugins/
 
 echo -e '\n\e[34m\e[1m##### Run tests on php 5.2 #####\e[0m\n'
-codecept run functional -g php5.2 --fail-fast
+codecept run functional -g php5.2 --env=$DOCKER_ENV --fail-fast
 
 function do_tests () {
     echo -e "\n\e[34m\e[1m##### Run tests on php $1 #####\e[0m\n"
     mysql -uroot -h $MYSQL_IP -e "DELETE FROM wp_options WHERE option_value LIKE 'advgb_%';" wordpress
     sshpass -p 'password' ssh -q -o PreferredAuthentications=password -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -a -p 2222 root@$WWW_IP "/root/set_php_version.sh $1; cd /var/www/html/; wp plugin deactivate --allow-root advanced-gutenberg; wp post delete --allow-root \$(wp post list --allow-root --post_type='advgb_profiles' --format=ids) || true; wp post delete --allow-root \$(wp post list --allow-root --post_type='post' --format=ids) || true"
-    codecept run functional --skip-group php5.2 --fail-fast
-    codecept run acceptance --skip-group php5.2 --fail-fast
+    codecept run functional --skip-group php5.2 --env=$DOCKER_ENV --fail-fast -o "php-version: $1"
+    codecept run acceptance --skip-group php5.2 --env=$DOCKER_ENV --fail-fast -o "php-version: $1"
+
+    # Check for php errors
+    sshpass -p 'password' scp -q -r -o PreferredAuthentications=password -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -P 2222 root@$WWW_IP:/var/www/html/wp-content/debug.log "$PLUGIN_DIR/tests/_output/wp_debug.log" 2>/dev/null || true
+    echo -e "\n\e[1mChecking errors in debug.log file\e[0m"
+    PHP_PLUGIN_ERRORS=$(grep "/var/www/html/wp-content/plugins/advanced-gutenberg" "$PLUGIN_DIR/tests/_output/wp_debug.log" 2>/dev/null || true)
+    if [[ "$PHP_PLUGIN_ERRORS" != "" ]]; then
+        echo -e "\n\e[31m\e[1m\xe2\x9c\x95 Php errors related to the plugin found in the debug log file\e[0m\n"
+        echo "$PHP_PLUGIN_ERRORS"
+        exit 1
+    else
+        echo -e "\n\e[32m\e[1m\xe2\x9c\x93 No php errors related to the plugin found in the debug log file\e[0m\n"
+    fi
+
+    PHP_ERRORS=$(grep -v "/var/www/html/wp-content/plugins/advanced-gutenberg" "$PLUGIN_DIR/tests/_output/wp_debug.log" 2>/dev/null || true)
+    if [[ "$PHP_ERRORS" != "" ]]; then
+        echo -e "\n\e[31m\e[! Php errors not related to the plugin found in the debug log file\e[0m\n"
+        echo "$PHP_ERRORS"
+    else
+        echo -e "\n\e[32m\e[1m\xe2\x9c\x93 No php errors not related to the plugin found in the debug log file\e[0m\n"
+    fi
 }
 
 for PHP_VERSION in '5.3' '5.4' '5.5' '5.6' '7.0' '7.1' '7.2'
--- tests/scripts/local_codeception.sh
+++ tests/scripts/local_codeception.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-#
-# Run codeception tests under local docker images
-#
-
-# cd to main plugin directory
+# Change directory to main plugin directory
 cd "$(dirname "$0")/../../"
+PLUGIN_DIR=$(pwd)
 
-#Clean previous run
+# Clean previous run
 echo -e '\e[34m\e[1m##### Remove docker containers #####\e[0m\n'
+# Disconnect before remove it
+for CONTAINER in "wordpress_cgi" "mysql" "chromedriver"
+do
+    docker network disconnect -f pipelines $CONTAINER
+done;
 docker rm -f wordpress_cgi mysql chromedriver
 docker network rm pipelines
 
@@ -16,12 +18,23 @@ MYSQL_IP=172.20.0.2
 WWW_IP=172.20.0.3
 CHROMEDRIVER_IP=172.20.0.4
 
+# Start chrome driver locally if docker env variable passed as first argument
+if [ -z "$1" ]; then
+    DOCKER_ENV="local"
+elif [ "$1" = "local_chrome" ]; then
+    DOCKER_ENV="$1"
+    killall chromedriver
+    chromedriver --url-base=/wd/hub --port=4444 &
+else
+    DOCKER_ENV="$1"
+fi
+
 # Start containers
 echo -e '\n\e[34m\e[1m##### Start docker containers #####\e[0m\n'
-docker network create --subnet=172.20.0.0/16 pipelines
+docker network create --subnet=172.20.0.0/24 pipelines
 docker run --name mysql -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -d --network pipelines --ip $MYSQL_IP mysql:5.7.22
 docker run --name wordpress_cgi --link mysql -d -e MYSQL_HOST=mysql -e WWW_HOST=$WWW_IP --network=pipelines --ip $WWW_IP joomunited/wordpress_cgi:latest
-docker run --name chromedriver --network=pipelines --ip $CHROMEDRIVER_IP -d blueimp/chromedriver
+docker run --name chromedriver --network=pipelines --ip $CHROMEDRIVER_IP -d joomunited/chromedriver:latest
 
 max_steps=20
 time_to_wait=5
@@ -35,7 +48,7 @@ for i in `seq 1 $max_steps`; do
 		sleep $time_to_wait
 done
 
-if [[ $i == 30 ]]; then
+if [[ $i == $max_steps ]]; then
   echo "Mysql server is not available stopping"
   exit 1
 fi
@@ -51,7 +64,7 @@ for i in `seq 1 $max_steps`; do
         sleep $time_to_wait
 done
 
-if [[ $i == 30 ]]; then
+if [[ $i == $max_steps ]]; then
   echo "Web server is not available stopping"
   exit 1
 fi
@@ -60,8 +73,14 @@ echo "Web server ready"
 
 set -e
 
+# Clean plugin folder to made sure tests script is updated
+sshpass -p 'password' ssh -q -o PreferredAuthentications=password -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -a -p 2222 root@$WWW_IP "rm -fr /var/www/html/wp-content/plugins/advanced-gutenberg; mkdir -p /var/www/html/wp-content/plugins/advanced-gutenberg"
 # Copy plugin to web server
-sshpass -p 'password' ssh -q -o PreferredAuthentications=password -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -a -p 2222 root@$WWW_IP "mkdir -p /var/www/html/wp-content/plugins/advanced-gutenberg/"
-sshpass -p 'password' scp -q -r -o PreferredAuthentications=password -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -P 2222 "$( pwd )" root@$WWW_IP:/var/www/html/wp-content/plugins/
+sshpass -p 'password' ssh -q -o PreferredAuthentications=password -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -a -p 2222 root@$WWW_IP "mkdir -p /var/www/html/wp-content/plugins/advanced-gutenberg"
+sshpass -p 'password' scp -q -r -o PreferredAuthentications=password -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -P 2222 "$PLUGIN_DIR"/* root@$WWW_IP:/var/www/html/wp-content/plugins/advanced-gutenberg
 
-source ./tests/scripts/codeception.sh
+# Fix chown
+sshpass -p 'password' ssh -q -o PreferredAuthentications=password -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -a -p 2222 root@$WWW_IP "chown -Rh www-data:www-data /var/www/html/wp-content/plugins/advanced-gutenberg"
+
+cd "$PLUGIN_DIR"
+source "$PLUGIN_DIR/tests/scripts/codeception.sh"
