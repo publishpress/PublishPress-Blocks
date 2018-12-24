7ebc2b459c07ee079e2cd27443bc3085a1762f6e
Linh LV <linh.codeunited@gmail.com>
2018-12-24 15:19:26 +0700

    Remove custom columns

--- assets/blocks/custom-columns/columns.jsx
+++ assets/blocks/custom-columns/columns.jsx
@@ -28,45 +28,24 @@
     addFilter( 'editor.BlockEdit', 'advgb/editColumnsAttrs', function ( BlockEdit ) {
         return ( props ) => {
             if (props.name === "core/text-columns" || props.name === "core/columns") {
-                const { isSelected, attributes, setAttributes, clientId } = props;
-                const { colMargin, colPadding, blockID } = attributes;
+                const { attributes, clientId } = props;
+                const { colMargin, colPadding } = attributes;
 
-                return ( [
-                    <BlockEdit key="block-edit-custom-columns" {...props}/>,
-                    isSelected &&
-                    <InspectorControls key="inspector-custom-columns">
-                        <RangeControl
-                            label={ __( 'Columns margin' ) }
-                            value={ colMargin }
-                            min={ 0 }
-                            max={ 200 }
-                            onChange={ (value) => {
-                                if (!blockID) setAttributes( { blockID: 'columns-' + clientId } );
-                                return setAttributes( { colMargin: value } );
-                            } }
-                        />
-                        <RangeControl
-                            label={ __( 'Columns padding' ) }
-                            value={ colPadding }
-                            min={ 0 }
-                            max={ 100 }
-                            onChange={ (value) => {
-                                if (!blockID) setAttributes( { blockID: 'columns-' + clientId } );
-                                return setAttributes( { colPadding: value } );
-                            } }
-                        />
-                    </InspectorControls>,
-                    props.name === 'core/columns' && (!!colMargin || !!colPadding) &&
-                    <style key="custom-columns-styles">
-                        {`#block-${clientId} .wp-block-columns .editor-block-list__block:not(:first-child) {margin-left: ${colMargin}px;}`}
-                        {`#block-${clientId} .wp-block-columns .editor-block-list__block-edit {padding: ${colPadding}px;}`}
-                    </style>,
-                    props.name === 'core/text-columns' && (!!colMargin || !!colPadding) &&
-                    <style key="custom-text-columns-styles">
-                        {`#block-${clientId} .wp-block-column:not(:first-child) {margin-left: ${colMargin}px;}`}
-                        {`#block-${clientId} .wp-block-column {padding: ${colPadding}px;}`}
-                    </style>
-                ] )
+                return (
+                    <Fragment>
+                        <BlockEdit {...props} />
+                        {props.name === 'core/columns' && (!!colMargin || !!colPadding) &&
+                        <style key="custom-columns-styles">
+                            {`#block-${clientId} .wp-block-columns .editor-block-list__block:not(:first-child) {margin-left: ${colMargin}px;}`}
+                            {`#block-${clientId} .wp-block-columns .editor-block-list__block-edit {padding: ${colPadding}px;}`}
+                        </style>},
+                        {props.name === 'core/text-columns' && (!!colMargin || !!colPadding) &&
+                        <style key="custom-text-columns-styles">
+                            {`#block-${clientId} .wp-block-column:not(:first-child) {margin-left: ${colMargin}px;}`}
+                            {`#block-${clientId} .wp-block-column {padding: ${colPadding}px;}`}
+                        </style>}
+                    </Fragment>
+                )
             }
 
             return <BlockEdit {...props} />;
@@ -75,11 +54,11 @@
 
     // Save options to show in frontend
     addFilter( 'blocks.getSaveContent.extraProps', 'advgb/saveColumnsAttrs', function ( extraProps, blockType, attributes ) {
-        const { blockID } = attributes;
+        const { colMargin, colPadding, blockID } = attributes;
 
         if (blockType.name === 'core/text-columns' || blockType.name === 'core/columns') {
             extraProps = Object.assign( extraProps, {
-                id: blockID,
+                id: (colMargin || colPadding) ? blockID : extraProps.id,
             } )
         }
 
