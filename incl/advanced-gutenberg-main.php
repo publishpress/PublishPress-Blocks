efbba531efd8171d5e9c897124271bdbf705bb47
Linh LV <linh.codeunited@gmail.com>
2019-04-01 15:40:36 +0700

    Load accordions with collapsed option on frontend

--- incl/advanced-gutenberg-main.php
+++ incl/advanced-gutenberg-main.php
@@ -3211,10 +3211,13 @@ float: left;'
         if (strpos($content, 'advgb-accordion-block') !== false) {
             wp_enqueue_script('jquery-ui-accordion');
             wp_add_inline_script('jquery-ui-accordion', 'jQuery(document).ready(function($){
-                $(".advgb-accordion-block").parent().accordion({
-                    header: ".advgb-accordion-header",
-                    heightStyle: "content",
-                    collapsible: true,
+                $(".advgb-accordion-block").parent().each(function() {
+                    $(this).accordion({
+                        header: ".advgb-accordion-header",
+                        heightStyle: "content",
+                        collapsible: true,
+                        active: $(this).find(".advgb-accordion-block:first").data("collapsed") ? false : 0,
+                    });
                 });
             });');
         }
