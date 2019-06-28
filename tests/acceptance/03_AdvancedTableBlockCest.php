76ede43a951f761bb43a4da7cedcb7a5a331af39
Linh LV <linh.codeunited@gmail.com>
2019-06-28 12:31:16 +0700

    [Test] Fix error while click view post

--- tests/acceptance/03_AdvancedTableBlockCest.php
+++ tests/acceptance/03_AdvancedTableBlockCest.php
@@ -62,7 +62,7 @@ class AdvancedTableBlockCest
         $I->click('Publish');
         $I->waitForText('Post published.');
 
-        $I->click('View Post');
+        $I->clickViewPost();
 
         $I->seeElement('.wp-block-advgb-table.advgb-table-frontend');
         $I->seeNumberOfElements('.advgb-table-frontend td', 16);
@@ -99,7 +99,7 @@ class AdvancedTableBlockCest
         $I->click('Update');
         $I->waitForText('Post updated.');
 
-        $I->click('View Post');
+        $I->clickViewPost();
 
         $I->waitForElement('.advgb-table-frontend');
 
@@ -167,7 +167,7 @@ class AdvancedTableBlockCest
         $I->click('Update');
         $I->waitForText('Post updated.');
 
-        $I->click('View Post');
+        $I->clickViewPost();
 
         $I->waitForElementVisible('.advgb-table-frontend', 5);
         $I->seeElement('//table[contains(@class,"advgb-table-frontend")]//tr[1]//td[3 and contains(@style, "background-color:'.$colors[0].'")]');
@@ -205,7 +205,7 @@ class AdvancedTableBlockCest
         $I->click('Update');
         $I->waitForText('Post updated.');
 
-        $I->click('View Post');
+        $I->clickViewPost();
 
         $I->seeNumberOfElements('.advgb-table-frontend td', 16);
 
@@ -229,7 +229,7 @@ class AdvancedTableBlockCest
         $I->click('Update');
         $I->waitForText('Post updated.');
 
-        $I->click('View Post');
+        $I->clickViewPost();
 
         // Check the actual width
         $width = $I->getElementWidth('//*[contains(@class,"advgb-table-frontend")]');
