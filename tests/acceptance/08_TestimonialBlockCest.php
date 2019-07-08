5495c4e78b02773a3c066d5a0c0c80968379aeb5
Linh LV <linh.codeunited@gmail.com>
2019-07-08 10:14:59 +0700

    [Add] Change testimonial text test

--- tests/acceptance/08_TestimonialBlockCest.php
+++ tests/acceptance/08_TestimonialBlockCest.php
@@ -37,6 +37,21 @@ class TestimonialBlockCest
 
         $I->waitForElement('//div[contains(@class, "advgb-testimonial")]');
 
+        // Change person name
+        $I->click('.advgb-testimonial .advgb-testimonial-item h4.advgb-testimonial-name');
+        $I->selectCurrentElementText();
+        $I->pressKeys('John Doe');
+
+        // Change person position
+        $I->click('.advgb-testimonial .advgb-testimonial-item p.advgb-testimonial-position');
+        $I->selectCurrentElementText();
+        $I->pressKeys('Developer');
+
+        // Change person description
+        $I->click('.advgb-testimonial .advgb-testimonial-item p.advgb-testimonial-desc');
+        $I->selectCurrentElementText();
+        $I->pressKeys('Coding is the passion of his life.');
+
         $I->click('Publish…');
         $I->waitForElementVisible('.editor-post-publish-button');
 
@@ -48,5 +63,8 @@ class TestimonialBlockCest
         // Check block loaded
         $I->waitForElement('.wp-block-advgb-testimonial');
         $I->seeElement('.advgb-testimonial-item');
+        $I->seeElement('//div[@class="advgb-testimonial-item"]/h4[@class="advgb-testimonial-name"][text()="John Doe"]');
+        $I->seeElement('//div[@class="advgb-testimonial-item"]/p[@class="advgb-testimonial-position"][text()="Developer"]');
+        $I->seeElement('//div[@class="advgb-testimonial-item"]/p[@class="advgb-testimonial-desc"][text()="Coding is the passion of his life."]');
     }
 }
