b19f5e17d1626b38c0e05406a7eb22b8d9f8c79b
Linh LV <linh.codeunited@gmail.com>
2019-07-16 10:17:01 +0700

    [Add] Products block test: change number of items

--- tests/acceptance/16_ProductsBlockCest.php
+++ tests/acceptance/16_ProductsBlockCest.php
@@ -54,4 +54,20 @@ class ProductsBlockCest
         $I->seeElement('.advgb-woo-products .woocommerce.columns-3 .products.columns-3');
         $I->seeNumberOfElements('//div[contains(@class, "woocommerce")]/ul[contains(@class, "products")]/li[contains(@class, "product")]', 6);
     }
+
+    public function changeNumberOfItems(AcceptanceTester $I)
+    {
+        $I->wantTo('Change number of products shown');
+
+        // Change
+        $I->fillField('//label[text()="Columns"]/following-sibling::node()/following-sibling::node()', 2);
+        $I->fillField('//label[text()="Number of Products"]/following-sibling::node()/following-sibling::node()', 4);
+
+        $I->updatePost();
+        $I->waitForElement('.wp-block-advgb-woo-products');
+
+        // Check
+        $I->seeElement('.advgb-woo-products .woocommerce.columns-2 .products.columns-2');
+        $I->seeNumberOfElements('//div[contains(@class, "woocommerce")]/ul[contains(@class, "products")]/li[contains(@class, "product")]', 4);
+    }
 }
