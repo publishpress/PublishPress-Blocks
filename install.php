d58ef2f533671586b491cfe9376364417639a6b2
Linh LV <linh.codeunited@gmail.com>
2019-07-19 10:13:51 +0700

    Hide Container block by default when update to 2.0.6

--- install.php
+++ install.php
@@ -113,7 +113,7 @@ register_activation_hook(ADVANCED_GUTENBERG_PLUGIN, function () {
 $advgb_current_version = get_option('advgb_version', '0.0.0');
 global $wpdb;
 
-if (version_compare($advgb_current_version, '1.6.7', 'lt')) {
+if (version_compare($advgb_current_version, '2.0.6', 'lt')) {
     $all_blocks_list = get_option('advgb_blocks_list');
     if (!is_array($all_blocks_list)) {
         $all_blocks_list     = array();
@@ -125,37 +125,18 @@ if (version_compare($advgb_current_version, '1.6.7', 'lt')) {
 
     if (!empty($profiles)) {
         foreach ($profiles as $profile) {
-            $active_blocks_saved = get_post_meta($profile->ID, 'active_blocks', true);
-            $isNewProfile = get_post_meta($profile->ID, 'blocks', true);
+            $blocks_saved = get_post_meta($profile->ID, 'blocks', true);
 
-            // Active all blocks from default profiles
-            if (!is_array($active_blocks_saved)) {
-                if ($active_blocks_saved === 'all' ||
-                    $isNewProfile && isset($isNewProfile['active_blocks']) && count($isNewProfile['active_blocks']) < 1) {
-                    update_post_meta($profile->ID, 'blocks', array('active_blocks'   => array(), 'inactive_blocks' => array()));
-                    delete_post_meta($profile->ID, 'active_blocks');
-                    continue;
-                }
-            }
-
-            // Check if it already is a new profile, no need to update it
-            if ($isNewProfile && isset($isNewProfile['active_blocks'])) {
+            if (!is_array($blocks_saved)) {
                 continue;
             }
 
-            if (!is_array($active_blocks_saved)) {
-                $active_blocks_saved     = array();
-            }
-            // Rewrite the $all_block_list array to a simple index array with only block name
-            $all_blocks = array();
-            foreach ($all_blocks_list as $all_block_list) {
-                $all_blocks[] = $all_block_list['name'];
-            }
-            $inactive_blocks = array_diff($all_blocks, $active_blocks_saved);
-            $inactive_blocks = array_values($inactive_blocks);
+            // Remove Container block from profile
+            $key = array_search('advgb/container', $blocks_saved['active_blocks']);
+            unset($blocks_saved['active_blocks'][$key]);
+            array_push($blocks_saved['inactive_blocks'], 'advgb/container');
 
-            update_post_meta($profile->ID, 'blocks', array('active_blocks'=>$active_blocks_saved, 'inactive_blocks'=>$inactive_blocks));
-            delete_post_meta($profile->ID, 'active_blocks');
+            update_post_meta($profile->ID, 'blocks', $blocks_saved);
         }
     }
 
