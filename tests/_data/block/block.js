a72d8eea617aa241bf701bd4490e5f178a665950
Tien <ptiencntt94@gmail.com>
2019-12-04 17:31:07 +0700

    [Remove] Wordpress version 5.1 [Remove] PHP 5.2, 5.6 [Update] Test block

--- tests/_data/block/block.js
+++ tests/_data/block/block.js
@@ -1,19 +1,34 @@
-var el = wp.element.createElement,
-    registerBlockType = wp.blocks.registerBlockType,
-    blockStyle = { backgroundColor: '#900', color: '#fff', padding: '20px' };
 
-registerBlockType( 'gutenberg-test-block/test', {
-    title: 'Test block',
+( function( blocks, element ) {
+    var el = element.createElement;
 
-    icon: 'universal-access-alt',
+    var blockStyle = {
+        backgroundColor: '#900',
+        color: '#fff',
+        padding: '20px',
+    };
 
-    category: 'layout',
-
-    edit: function() {
-        return el( 'p', { style: blockStyle }, 'Test block.' );
-    },
-
-    save: function() {
-        return el( 'p', { style: blockStyle }, 'Test block saved content.' );
-    },
-});
+    blocks.registerBlockType( 'gutenberg-test-block/test', {
+        title: 'Test block',
+        icon: 'universal-access-alt',
+        category: 'layout',
+        example: {},
+        edit: function() {
+            return el(
+                'p',
+                { style: blockStyle },
+                'Test block.'
+            );
+        },
+        save: function() {
+            return el(
+                'p',
+                { style: blockStyle },
+                'Test block saved content.'
+            );
+        },
+    } );
+}(
+    window.wp.blocks,
+    window.wp.element
+) );
