370c7ad99d584718bbea429162ed065acb2e74ff
Valentín García <htmgarcia@gmail.com>
2020-05-27 18:46:03 -0500

    Placeholder logo

--- incl/view/advanced-gutenberg-main-view.php
+++ incl/view/advanced-gutenberg-main-view.php
@@ -43,7 +43,7 @@ $tabs_data = array(
     <div class="ju-left-panel">
         <div class="ju-logo">
             <a href="https://advancedgutenberg.com/" target="_blank">
-                <img src="<?php echo esc_url(plugins_url('assets/images/logo-joomUnited-white.png', dirname(dirname(__FILE__)))) ?>"
+                <img src="<?php echo esc_url(plugins_url('assets/images/logo-white.png', dirname(dirname(__FILE__)))) ?>"
                      alt="<?php esc_html_e('JoomUnited logo', 'advanced-gutenberg') ?>">
             </a>
         </div>
