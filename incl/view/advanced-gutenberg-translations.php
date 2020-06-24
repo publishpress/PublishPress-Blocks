1eb35d775ea401a664309f8ebcc0d97bcd9f3edf
Valentín García <htmgarcia@gmail.com>
2020-06-24 11:59:34 -0500

    Use plural for translation word
    #38

---
+++ incl/view/advanced-gutenberg-translations.php
@@ -0,0 +1,10 @@
+<?php
+defined('ABSPATH') || die;
+?>
+
+<div class="advgb-header" style="padding-top: 40px">
+    <h1 class="advgb-header"><?php esc_html_e('Translations', 'advanced-gutenberg') ?></h1>
+</div>
+<div>
+    <?php _e('[Information about the new translation system goes here]', 'advanced-gutenberg'); ?>
+</div>
