8a46a3434ac7dd2232e6e587edcb03a99a1851c4
Valentín García <htmgarcia@gmail.com>
2020-06-24 14:28:55 -0500

    Some explanatory text for translations page
    #38

--- incl/view/advanced-gutenberg-translations.php
+++ incl/view/advanced-gutenberg-translations.php
@@ -3,8 +3,10 @@ defined('ABSPATH') || die;
 ?>
 
 <div class="advgb-header" style="padding-top: 40px">
-    <h1 class="advgb-header"><?php esc_html_e('Translations', 'advanced-gutenberg') ?></h1>
+    <h1 class="header-title"><?php esc_html_e('Translations', 'advanced-gutenberg') ?></h1>
 </div>
+
 <div>
-    <?php _e('[Information about the new translation system goes here]', 'advanced-gutenberg'); ?>
+    <p><?php _e('Support for JUTranslation system has been removed, however we\'ll provide you a better approach to continue translating Advanced Gutenberg into your language.', 'advanced-gutenberg'); ?></p>
+    <p><?php _e('[Information about the new translation approach goes here]', 'advanced-gutenberg'); ?></p>
 </div>
