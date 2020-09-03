610424b798adf435b368aae686f9a607c3d0a9bc
Valentín García <htmgarcia@gmail.com>
2020-09-03 10:54:49 -0500

    Increase version

--- src/advanced-gutenberg.php
+++ src/advanced-gutenberg.php
@@ -3,7 +3,7 @@
  * Plugin Name: Advanced Gutenberg
  * Plugin URI: https://advancedgutenberg.com
  * Description: Enhanced tools for Gutenberg editor
- * Version: 2.4.5b2
+ * Version: 2.4.5
  * Tested up to: 5.5.1
  * Author: Advanced Gutenberg
  * Author URI: https://advancedgutenberg.com
@@ -75,7 +75,7 @@ if (version_compare(PHP_VERSION, '5.6.20', '<')) {
 }
 
 if (! defined('ADVANCED_GUTENBERG_VERSION')) {
-    define('ADVANCED_GUTENBERG_VERSION', '2.4.5b2');
+    define('ADVANCED_GUTENBERG_VERSION', '2.4.5');
 }
 
 if (! defined('ADVANCED_GUTENBERG_PLUGIN')) {
