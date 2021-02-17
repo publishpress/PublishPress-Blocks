1ce6b1c8a3e18cd439ded713a8e76da2bbfd816b
Valentín García <htmgarcia@gmail.com>
2021-02-17 13:17:39 -0600

    Remove redundant code related to columns visual guides

--- src/incl/advanced-gutenberg-main.php
+++ src/incl/advanced-gutenberg-main.php
@@ -1440,17 +1440,6 @@ if(!class_exists('AdvancedGutenbergMain')) {
                         '#editor div.block-editor-writing-flow {margin: 0 auto} #editor .wp-block:not([data-type="advgb/images-slider"]):not([data-type="advgb/testimonial"]) {max-width: inherit}'
                     );
                 }
-
-                if (!isset($saved_settings['enable_columns_visual_guide'])
-                    || (isset($saved_settings['enable_columns_visual_guide']) && $saved_settings['enable_columns_visual_guide'])
-                ) {
-                    if($saved_settings['enable_columns_visual_guide'] == '1') {
-                        wp_add_inline_style(
-                            'dashicons',
-                            '.advgb-columns>.block-editor-inner-blocks>.block-editor-block-list__layout>.wp-block:not(.is-selected)>.advgb-column{border:1px dashed #999;}'
-                        );
-                    }
-                }
             }
         }
 
