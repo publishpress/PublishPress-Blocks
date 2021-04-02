2d0889d7860b0bf7de491582f5bc39f77a67498a
Valentín García <htmgarcia@gmail.com>
2021-04-02 14:02:36 -0600

    Increase version

--- src/advanced-gutenberg.php
+++ src/advanced-gutenberg.php
@@ -3,7 +3,7 @@
  * Plugin Name: PublishPress Blocks
  * Plugin URI: https://publishpress.com/blocks/
  * Description: Enhanced tools for Gutenberg editor
- * Version: 2.5.8b8
+ * Version: 2.5.8b9
  * Tested up to: 5.7
  * Author: PublishPress
  * Author URI: https://publishpress.com/
@@ -39,7 +39,7 @@
 defined('ABSPATH') || die;
 
 if (! defined('ADVANCED_GUTENBERG_VERSION')) {
-    define('ADVANCED_GUTENBERG_VERSION', '2.5.8b8');
+    define('ADVANCED_GUTENBERG_VERSION', '2.5.8b9');
 }
 
 // Code shared with Pro version
