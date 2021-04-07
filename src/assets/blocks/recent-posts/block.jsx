55052b5677fc7ad04a7504218360e8d718b90eef
Valentín García <htmgarcia@gmail.com>
2021-04-07 14:40:57 -0500

    2 new Newspaper irregular layouts with leading post and 2 columns below

--- src/assets/blocks/recent-posts/block.jsx
+++ src/assets/blocks/recent-posts/block.jsx
@@ -72,6 +72,8 @@ import AdvQueryControls from './query-controls.jsx';
         { layout: 'np-1-4', icon: 'np-1-4', title: __( 'One leading post in the left, 4 posts in the right', 'advanced-gutenberg' ) },
         { layout: 'np-1-5', icon: 'np-1-5', title: __( 'One leading post in the left, 5 posts in the right', 'advanced-gutenberg' ) },
         { layout: 'np-2', icon: 'np-2', title: __( 'One leading post on top, below the rest of posts', 'advanced-gutenberg' ) },
+        { layout: 'np-3-2', icon: 'np-3-2', title: __( 'One leading post on top, below 2 columns with 1 post in the left and 2 posts in the right', 'advanced-gutenberg' ) },
+        { layout: 'np-3-3', icon: 'np-3-3', title: __( 'One leading post on top, below 2 columns with 1 post in the left and 3 posts in the right', 'advanced-gutenberg' ) },
     ];
 
     let initSlider = null;
