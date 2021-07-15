773c56f2ec640008c258bc57567b6905413b355a
Valentín García <htmgarcia@gmail.com>
2021-07-15 10:25:15 -0500

    Update lightbox-customizer.js

--- src/assets/blocks/advimage/lightbox-customizer.js
+++ src/assets/blocks/advimage/lightbox-customizer.js
@@ -1,16 +1,20 @@
 jQuery(document).ready(function ($) {
     document.addEventListener("DOMSubtreeModified", function(){
-        $('.advgb-lightbox').colorbox({
-            title: function () {
-                return $(this).find('.advgb-image-title').text();
-            },
-            maxWidth: '90%',
-            maxHeight: '85%',
-            fixed: true,
-            className: 'advgb_lightbox',
-            href: function () {
-                return $(this).data('image');
-            }
-        });
+        try {
+            $('.advgb-lightbox').colorbox({
+                title: function () {
+                    return $(this).find('.advgb-image-title').text();
+                },
+                maxWidth: '90%',
+                maxHeight: '85%',
+                fixed: true,
+                className: 'advgb_lightbox',
+                href: function () {
+                    return $(this).data('image');
+                }
+            });
+        } catch(e) {
+            // console.error(e);
+        }
     });
 });
