78c7cc7174ce6ee412d243629a056c086cd2d0a5
Valentín García <htmgarcia@gmail.com>
2021-09-10 12:58:32 -0500

    Add setting to disable Block Access

--- src/incl/view/advanced-gutenberg-main-view.php
+++ src/incl/view/advanced-gutenberg-main-view.php
@@ -7,33 +7,38 @@ if (!function_exists('register_block_type') && !defined('GUTENBERG_DEVELOPMENT_M
 }
 $phpver = phpversion();
 
-$tabs_data = array(
-    array(
+// Block Access tab
+$saved_settings = get_option('advgb_settings');
+//var_dump( $saved_settings );
+if( !isset($saved_settings['enable_block_access']) || $saved_settings['enable_block_access'] ) {
+    $tabs_data[] = [
         'id' => 'block-access',
         'title' => __('Block Access', 'advanced-gutenberg'),
         'icon' => 'account-circle',
-    ),
-    array(
-        'id' => 'settings',
-        'title' => __('Settings', 'advanced-gutenberg'),
-        'icon' => 'build',
-    ),
-    array(
-        'id' => 'block-settings',
-        'title' => __('Block Settings', 'advanced-gutenberg'),
-        'icon' => 'settings',
-    ),
-    array(
-        'id' => 'email-form',
-        'title' => __('Email & Form', 'advanced-gutenberg'),
-        'icon' => 'mail',
-    ),
-    array(
-        'id' => 'custom-styles',
-        'title' => __('Custom Styles', 'advanced-gutenberg'),
-        'icon' => 'code',
-    ),
-);
+    ];
+}
+
+// Rest of tabs
+$tabs_data[] = [
+    'id' => 'settings',
+    'title' => __('Settings', 'advanced-gutenberg'),
+    'icon' => 'build',
+];
+$tabs_data[] = [
+    'id' => 'block-settings',
+    'title' => __('Block Settings', 'advanced-gutenberg'),
+    'icon' => 'settings',
+];
+$tabs_data[] = [
+    'id' => 'email-form',
+    'title' => __('Email & Form', 'advanced-gutenberg'),
+    'icon' => 'mail',
+];
+$tabs_data[] = [
+    'id' => 'custom-styles',
+    'title' => __('Custom Styles', 'advanced-gutenberg'),
+    'icon' => 'code',
+];
 
 // Upgrade to Pro page
 if(!defined('ADVANCED_GUTENBERG_PRO')) {
