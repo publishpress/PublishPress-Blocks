715f0d8bd5776f8bdb24639772dea9cfe70d19e0
Valentín García <htmgarcia@gmail.com>
2021-10-26 15:34:21 -0500

    Init review class and adjust logo path

--- src/review/review-request.php
+++ src/review/review-request.php
@@ -15,7 +15,7 @@ if( !class_exists('AdvancedGutenberg_Reviews') ) {
 	        $this->reviewController = new ReviewsController(
 	            'advanced-gutenberg',
 	            'PublishPress Blocks',
-	            ADVANCED_GUTENBERG_PLUGIN . '/assets/images/logo-notice.png'
+	            esc_url(plugins_url('assets/images/logo-notice.png', dirname(__FILE__)))
 	        );
 	    }
 
@@ -28,46 +28,18 @@ if( !class_exists('AdvancedGutenberg_Reviews') ) {
 
 	    public function shouldDisplayBanner($shouldDisplay)
 	    {
-	        global $pagenow;
-
 	        if (! is_admin() || ! current_user_can('edit_posts')) {
 	            return false;
 	        }
 
-	        if ($pagenow === 'admin.php' && isset($_GET['page'])) {
-	            if ($_GET['page'] === 'pp-page1') {
-	                return true;
-	            }
-
-	            if ($_GET['page'] === 'pp-page2') {
-	                return true;
-	            }
-	        }
-
-	        if ($pagenow === 'edit.php' && isset($_GET['post_type'])) {
-	            if ($_GET['post_type'] === 'pp_custom_post_type') {
-	                return true;
-	            }
-	        }
-
-	        return false;
+	        return true;
 	    }
-
-		/*function advanced_gutenberg_wp_reviews_meta_map($metaMap)
-		{
-		    // You can override all the array, or specific keys.
-		    $metaMap = [
-		        'action_ajax_handler' => 'wp_ajax_advgb_review_action',
-		        'option_installed_on' => 'advgb_reviews_installed_on',
-		        'nonce_action' => 'advgb_review_action',
-		        'user_meta_dismissed_triggers' => '_advgb_reviews_dismissed_triggers',
-		        'user_meta_last_dismissed' => '_advgb_reviews_last_dismissed',
-		        'user_meta_already_did' => '_advgb_reviews_already_did',
-		        'filter_triggers' => '_advgb_reviews_triggers',
-		    ];
-
-		    return $metaMap;
-		}
-		add_filter('publishpress_wp_reviews_meta_map_advanced_gutenberg', 'advanced_gutenberg_wp_reviews_meta_map');*/
 	}
+
+	function advanced_gutenberg_ask_for_review()
+	{
+	    $review = new AdvancedGutenberg_Reviews;
+	    $review->init();
+	}
+	add_action('plugins_loaded', 'advanced_gutenberg_ask_for_review');
 }
