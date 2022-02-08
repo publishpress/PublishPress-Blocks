54a682697bf2d29d7b56127e6ef874b7132f9b4b
Valentín García <htmgarcia@gmail.com>
2022-02-08 10:50:45 -0600

    Delete commented code

--- src/assets/blocks/advaccordion/frontend.js
+++ src/assets/blocks/advaccordion/frontend.js
@@ -7,12 +7,4 @@ jQuery(document).ready(function($){
             active: $(this).data("collapsed") ? false : 0,
         });
     });
-    /*$(".advgb-accordion-block").parent().each(function() {
-        $(this).accordion({
-            header: "> div > .advgb-accordion-header",
-            heightStyle: "content",
-            collapsible: true,
-            active: $(this).find(".advgb-accordion-block:first").data("collapsed") ? false : 0,
-        });
-    });*/
 });
