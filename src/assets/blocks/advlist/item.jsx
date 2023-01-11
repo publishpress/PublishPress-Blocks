10425e1eff111dda411788e8b0b4800286d125d4
Valentín García <htmgarcia@gmail.com>
2023-01-11 14:26:36 -0600

    List item icon

--- src/assets/blocks/advlist/item.jsx
+++ src/assets/blocks/advlist/item.jsx
@@ -111,8 +111,7 @@
 
     const blockIcon = (
         <svg height="20" viewBox="2 2 22 22" width="20" xmlns="http://www.w3.org/2000/svg">
-            <path d="M3 13h2v-2H3v2zm0 4h2v-2H3v2zm0-8h2V7H3v2zm4 4h14v-2H7v2zm0 4h14v-2H7v2zM7 7v2h14V7H7z"/>
-            <path d="M0 0h24v24H0z" fill="none"/>
+            <path d="M 3,13 H 5 V 11 H 3 Z m 4,0 H 21 V 11 H 7 Z"/>
         </svg>
     );
 
--- src/assets/blocks/blocks.js
+++ src/assets/blocks/blocks.js
@@ -11165,8 +11165,7 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
     var blockIcon = React.createElement(
         'svg',
         { height: '20', viewBox: '2 2 22 22', width: '20', xmlns: 'http://www.w3.org/2000/svg' },
-        React.createElement('path', { d: 'M3 13h2v-2H3v2zm0 4h2v-2H3v2zm0-8h2V7H3v2zm4 4h14v-2H7v2zm0 4h14v-2H7v2zM7 7v2h14V7H7z' }),
-        React.createElement('path', { d: 'M0 0h24v24H0z', fill: 'none' })
+        React.createElement('path', { d: 'M 3,13 H 5 V 11 H 3 Z m 4,0 H 21 V 11 H 7 Z' })
     );
 
     var blockAttrs = {
