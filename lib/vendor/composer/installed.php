d167cdfef217b68a5849448474603817ff03b12f
Valentín García <htmgarcia@gmail.com>
2024-07-19 13:56:06 -0600

    Update composer
    #1394

--- lib/vendor/composer/installed.php
+++ lib/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-develop',
         'version' => 'dev-develop',
-        'reference' => 'fe55f94179c63047f455fc4ee307e96d52f296ca',
+        'reference' => '5e29109d82859df992050a6cf86450f456f06ee8',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-develop',
             'version' => 'dev-develop',
-            'reference' => 'fe55f94179c63047f455fc4ee307e96d52f296ca',
+            'reference' => '5e29109d82859df992050a6cf86450f456f06ee8',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
