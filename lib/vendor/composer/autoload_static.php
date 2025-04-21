da3247e4063020aad9b7ba0ab5d078c800c0e0ff
Olawale Adesina <olawale.olatech.pro@gmail.com>
2025-04-22 00:12:43 +0100

    Composer update

--- lib/vendor/composer/autoload_static.php
+++ lib/vendor/composer/autoload_static.php
@@ -8,10 +8,25 @@ class ComposerStaticInitPPBlocks
 {
     public static $files = array (
         '3b1e1688e889525de91ac2456aba9efd' => __DIR__ . '/..' . '/publishpress/psr-container/lib/include.php',
+        '04c6c5c2f7095ccf6c481d3e53e1776f' => __DIR__ . '/..' . '/mustangostang/spyc/Spyc.php',
         '24b27b1b9a32bf58eda571c3e5ae3480' => __DIR__ . '/..' . '/publishpress/pimple-pimple/lib/include.php',
         '0078757fbd019a5f202f2be6585c3626' => __DIR__ . '/..' . '/publishpress/wordpress-banners/BannersMain.php',
         '41c664bd04a95c2d6a2f2a3e00f06593' => __DIR__ . '/..' . '/publishpress/wordpress-reviews/ReviewsController.php',
         'a61bc28a742b9f9f2fd5ef4d2d1e2037' => __DIR__ . '/..' . '/publishpress/wordpress-version-notices/src/include.php',
+    );
+
+    public static $prefixLengthsPsr4 = array (
+        'D' => 
+        array (
+            'DeviceDetector\\' => 15,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'DeviceDetector\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/matomo/device-detector',
+        ),
     );
 
     public static $classMap = array (
@@ -21,6 +36,8 @@ class ComposerStaticInitPPBlocks
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInitPPBlocks::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitPPBlocks::$prefixDirsPsr4;
             $loader->classMap = ComposerStaticInitPPBlocks::$classMap;
 
         }, null, ClassLoader::class);
